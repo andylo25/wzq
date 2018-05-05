f0e383316e2e187fcea7fe80193ed81f815fc709
andy <andylo25@163.com>
2018-05-05 11:25:29 +0800

    f

--- gomoku/src/main/java/com/andy/gomoku/GomokuApplication.java
+++ gomoku/src/main/java/com/andy/gomoku/GomokuApplication.java
@@ -3,15 +3,12 @@ package com.andy.gomoku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.andy.gomoku.ai.WineAI;
-
 
 @SpringBootApplication
 //@EnableTransactionManagement
 public class GomokuApplication {
 	
 	public static void main(String[] args) {
-		new WineAI();
 		SpringApplication.run(GomokuApplication.class, args);
 	}
 }
